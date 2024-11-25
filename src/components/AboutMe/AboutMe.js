e405d2e537125e6f837790f5505a39135be72ff8
Yanss Villarreal <yancy_344@hotmail.com>
2024-11-25 13:44:46 +0100

    commit 02

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -6,16 +6,29 @@ export default function AboutMe() {
  const section = document.createElement('section')
  section.id = 'about'
  section.className = 'about'
+
  section.innerHTML = `
- <h2>Hello there!</h2>
- <div class="about-me-content">
-     <img src="${avatarSrc}" alt="Avatar" class="avatar">
-     <div class="about-me-text">
-         <p>Let me tell you a bit about myself... <br />
-         From fashion visionary to creative tech explorer! With a background in Image and Fashion Design, I spent years collaborating with fashion manufacturers, providing textile and design solutions that brought collections to life. Now, I'm diving into tech development and exploring the world of artificial intelligence, eager to combine creativity with code. My goal is to find a role that fuels my growth and lets me fully unleash my creative skills. Blending style with strategy, pixels with passion – I can’t wait to see where this journey leads!</p>
-     </div>
- </div>
-`
+          <h2>Hello there!</h2>
+          <img src="${avatarSrc}" alt="Avatar" class="avatar">
+          <p>
+            Let me tell you a bit about myself...
+            From fashion visionary to creative 
+            <span class="redirect-projects">
+              tech explorer!
+            </span>
+            With a background in Image and Fashion Design, I spent years collaborating with fashion manufacturers, providing textile and design solutions that brought collections to life. 
+            Now, I'm diving into tech development and exploring the world of artificial intelligence, eager to combine creativity with code. My goal is to find a role that fuels my growth and lets me fully unleash my creative skills.
+          </p>
+      `
+
+ section.addEventListener('click', (e) => {
+  if (e.target.classList.contains('redirect-projects')) {
+   const projectsSection = document.getElementById('projects')
+   if (projectsSection) {
+    projectsSection.scrollIntoView({ behavior: 'smooth' })
+   }
+  }
+ })
 
  return section
 }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,31 +11,31 @@ export default function Projects() {
       <div class="project">
         <a href="https://tiffany-anna-art-proyect1.netlify.app" target="_blank">
           <img src="./assets/DSC07074_copy_61fc8113-9c33-406c-aa2c-8c33cbf4f52f - copia.webp" />
-          <p>TIFFANY ANNA ART</p>
+          <p class="project-title">TIFFANY ANNA ART</p>
         </a>
       </div>
       <div class="project">
         <a href="https://practica-flex-entregafinal.netlify.app" target="_blank">
           <img src="./assets/free 3d shapes.png" alt="Project 2" />
-          <p>FREE 3D SHAPES</p>
+          <p class="project-title">FREE 3D SHAPES</p>
         </a>
       </div>
       <div class="project">
         <a href="https://fastprinting.netlify.app" target="_blank">
           <img src="./assets/fastprintngimg.png" />
-          <p>FAST PRINTING</p>
+          <p class="project-title">FAST PRINTING</p>
         </a>
       </div>
       <div class="project">
-        <a href="https://pinterestys.netlify.app/" target="_blank">
+        <a href="https://pinterestyvv.netlify.app" target="_blank">
           <img src="./assets/arq..png" />
-          <p>PINTEREST</p>
+          <p class="project-title">PINTEREST</p>
         </a>
       </div>
       <div class="project">
         <a href="#" target="_blank">
-          <img src="pendiente.jpg" alt="Project 5" />
-          <p>GAME</p>
+          <img src="./assets/comingsoon.png" />
+          <p class="project-title">GAME</p>
         </a>
       </div>
     </div>
